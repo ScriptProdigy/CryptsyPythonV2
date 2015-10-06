9f99256709bfb89028549803542a4b8b1144103f
Matt Smith <matt.joseph.smith@gmail.com>
2015-10-06 16:59:43 -0400

    No longer using variable id, renamed to obj_id

--- Cryptsy.py
+++ Cryptsy.py
@@ -12,11 +12,11 @@ class Cryptsy:
         self.PublicKey = PublicKey
         self.PrivateKey = PrivateKey
 
-    def _query(self, method, id=None, action=None, query=[], get_method="GET"):
+    def _query(self, method, obj_id=None, action=None, query=[], get_method="GET"):
 
         route = "/api/v2/" + method
-        if(id != None):
-            route = route + "/" + str(id)
+        if(obj_id != None):
+            route = route + "/" + str(obj_id)
             if(action != None):
                 route = route + "/" + str(action)
 
@@ -60,28 +60,28 @@ class Cryptsy:
     def markets(self):
         return self._query(method="markets")
 
-    def market(self, id):
-        return self._query(method="markets", id=id)
+    def market(self, obj_id):
+        return self._query(method="markets", obj_id=obj_id)
 
-    def market_orderbook(self, id, limit=100, otype="both", mine=False):
-        return self._query(method="markets", id=id, action="orderbook",
+    def market_orderbook(self, obj_id, limit=100, otype="both", mine=False):
+        return self._query(method="markets", obj_id=obj_id, action="orderbook",
             query=[("limit", limit), ("type", otype), ("mine", mine)])
 
-    def market_tradehistory(self, id, limit=100, mine=False):
-        return self._query(method="markets", id=id, action="tradehistory",
+    def market_tradehistory(self, obj_id, limit=100, mine=False):
+        return self._query(method="markets", obj_id=obj_id, action="tradehistory",
             query=[("limit", limit), ("mine", mine)])
 
-    def market_triggers(self, id, limit=100):
-        return self._query(method="markets", id=id, action="triggers",
+    def market_triggers(self, obj_id, limit=100):
+        return self._query(method="markets", obj_id=obj_id, action="triggers",
             query=[("limit", limit)])
 
-    def market_ohlc(self, id, start=0, stop=time.time(),
+    def market_ohlc(self, obj_id, start=0, stop=time.time(),
                                             interval="minute", limit=100):
         intervals = ["minute", "hour", "day"]
         if(interval not in intervals):
             interval = intervals[0]
         return self._query(method="markets",
-                           id=id,
+                           obj_id=obj_id,
                            action="ohlc",
                            query=[("start", start),
                                   ("stop", stop),
@@ -93,59 +93,59 @@ class Cryptsy:
     def currencies(self):
         return self._query(method="currencies")
 
-    def currency(self, id):
-        return self._query(method="currencies", id=id)
+    def currency(self, obj_id):
+        return self._query(method="currencies", obj_id=obj_id)
 
-    def currency_markets(self, id):
-        return self._query(method="currencies", id=id, action="markets")
+    def currency_markets(self, obj_id):
+        return self._query(method="currencies", obj_id=obj_id, action="markets")
 
 
     # User
     def balances(self, btype="all"):
         return self._query(method="balances", query=[("type", btype)])
 
-    def balance(self, id, btype="all"):
-        return self._query(method="balances", id=id, query=[("type", btype)])
+    def balance(self, obj_id, btype="all"):
+        return self._query(method="balances", obj_id=obj_id, query=[("type", btype)])
 
-    def deposits(self, id=0, limit=100):
-        if(id != 0):
+    def deposits(self, obj_id=0, limit=100):
+        if(obj_id != 0):
             return self._query(method="deposits",
-                               id=id,
+                               obj_id=obj_id,
                                query=[("limit", limit)])
         return self._query(method="deposits", query=[("limit", limit)])
 
-    def withdrawals(self, id=0, limit=100):
-        if(id != 0):
+    def withdrawals(self, obj_id=0, limit=100):
+        if(obj_id != 0):
             return self._query(method="withdrawals",
-                               id=id,
+                               obj_id=obj_id,
                                query=[("limit", limit)])
         return self._query(method="withdrawals", query=[("limit", limit)])
 
     def addresses(self):
         return self._query(method="addresses")
 
-    def address(self, id):
-        return self._query(method="addresses", id=id)
+    def address(self, obj_id):
+        return self._query(method="addresses", obj_id=obj_id)
 
     def transfers(self, limit=100):
         return self._query(method="transfers", query=[("limit", limit)])
 
 
     # Orders
-    def order(self, id):
-        return self._query(method="order", id=id)
+    def order(self, obj_id):
+        return self._query(method="order", obj_id=obj_id)
 
-    def order_create(self, marketid, quantity, ordertype, price):
+    def order_create(self, marketobj_id, quantity, ordertype, price):
         return self._query(method="order", query=[("quantity", quantity),
                                                     ("ordertype", ordertype),
                                                     ("price", price),
-                                                    ("marketid", marketid)],
+                                                    ("marketobj_id", marketobj_id)],
                                             get_method="POST")
 
-    def order_remove(self, id):
-        return self._query(method="order", id=id, get_method="DELETE")
+    def order_remove(self, obj_id):
+        return self._query(method="order", obj_id=obj_id, get_method="DELETE")
 
-    def order_move(self, id, price=False, quantity=False):
+    def order_move(self, obj_id, price=False, quantity=False):
         if(price == False and quantity == False):
             return {"success": False,
                     "error": {"ValueError": ["Must supply price and or quantity."]}}
@@ -159,19 +159,19 @@ class Cryptsy:
             if(isinstance(price, float) or isinstance(price, int)):
                 query.append(("price", price))
 
-        return self._query(method="order", id=id, action="move", 
+        return self._query(method="order", obj_id=obj_id, action="move", 
                                             query=query, 
                                             get_method="GET")
 
 
     # Triggers
-    def trigger(self, id):
-        return self._query(method="trigger", id=id)
+    def trigger(self, obj_id):
+        return self._query(method="trigger", obj_id=obj_id)
 
-    def trigger_create(self, marketid, ordertype, quantity,
+    def trigger_create(self, market_id, ordertype, quantity,
                                    comparison, price, orderprice, expires=''):
         return self._query(method="trigger",
-                           query=[("marketid", marketid),
+                           query=[("market_id", market_id),
                                   ("type", ordertype),
                                   ("quantity", quantity),
                                   ("comparison", comparison),
@@ -180,13 +180,13 @@ class Cryptsy:
                                   ("expires", expires)],
                            get_method="POST")
 
-    def trigger_remove(self, id):
-        return self._query(method="trigger", id=id, get_method="DELETE")
+    def trigger_remove(self, obj_id):
+        return self._query(method="trigger", obj_id=obj_id, get_method="DELETE")
 
 
     # Converter
-    def convert(self, id):
-        return self._query(method="converter", id=id)
+    def convert(self, obj_id):
+        return self._query(method="converter", obj_id=obj_id)
 
     def convert_create(self, fromcurrency, tocurrency, sendingamount=0.0,
                             receivingamount=0.0, tradekey="", feepercent=0.0):
